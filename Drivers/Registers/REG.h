4c81d534b775703ce6669f04ebecfd485eadbc21
zeiros20 <zizo.zizo2860@gmail.com>
2022-12-05 20:43:43 +0200

    Initial commit

---
+++ Drivers/Registers/REG.h
@@ -0,0 +1,75 @@
+#ifndef REG_H
+#define REG_H
+
+// All atmega32 register adresses with names defined for easy use
+
+#define     ADRR_SREG    0x5f
+#define     ADRR_SPH     0x5e
+#define     ADRR_SPL     0x5d
+#define     ADRR_OCR0    0x5c
+#define     ADRR_GICR    0x5b
+#define     ADRR_GIFR    0x5a
+#define     ADRR_TIMSK   0x59
+#define     ADRR_TIFR    0x58
+#define     ADRR_SPMCR   0x57
+#define     ADRR_TWCR    0x56
+#define     ADRR_MCUCR   0x55
+#define     ADRR_MCUCSR  0x54
+#define     ADRR_TCCR0   0x53
+#define     ADRR_TCNT0   0x52
+#define     ADRR_OSCCAL  0x51
+#define     ADRR_OCDR    0x51
+#define     ADRR_SFIOR   0x50
+#define     ADRR_TCCR1A  0x4f
+#define     ADRR_TCCR1B  0x4e
+#define     ADRR_TCNT1H  0x4d
+#define     ADRR_TCNT1L  0x4c
+#define     ADRR_OCR1AH  0x4b
+#define     ADRR_OCR1AL  0x4a
+#define     ADRR_OCR1BH  0x49
+#define     ADRR_OCR1Bl  0x48
+#define     ADRR_ICR1H   0x47
+#define     ADRR_ICR1L   0x46
+#define     ADRR_TCCR2   0x45
+#define     ADRR_TCNT2   0x44
+#define     ADRR_OCR2    0x43
+#define     ADRR_ASSR    0x42
+#define     ADRR_WDTCR   0x41
+#define     ADRR_UBRRH   0x40
+#define     ADRR_UCSRC   0x40
+#define     ADRR_EEARH   0x3f
+#define     ADRR_EEARL   0x3e
+#define     ADRR_EEDR    0x3d
+#define     ADRR_EECR    0x3c
+#define     ADRR_PORTA   0x3b
+#define     ADRR_DDRA    0x3a
+#define     ADRR_PINA    0x39
+#define     ADRR_PORTB   0x38
+#define     ADRR_DRRB    0x37
+#define     ADRR_PINB    0x36
+#define     ADRR_PORTC   0x35
+#define     ADRR_DRRC    0x34
+#define     ADRR_PINC    0x33
+#define     ADRR_PORTD   0x32
+#define     ADRR_DDRD    0x31
+#define     ADRR_PIND    0x30
+#define     ADRR_SPDR    0x2f
+#define     ADRR_SPSR    0x2e
+#define     ADRR_SPCR    0x2d
+#define     ADRR_UDR     0x2c
+#define     ADRR_UCSRA   0x2b
+#define     ADRR_UCSRB   0x2a
+#define     ADRR_UBRRL   0x29
+#define     ADRR_ACSR    0x28
+#define     ADRR_ADMUX   0x27
+#define     ADRR_ADCSRA  0x26
+#define     ADRR_ADCH    0x25
+#define     ADRR_ADCL    0x24
+#define     ADRR_TWDR    0x23
+#define     ADRR_TWAR    0x22
+#define     ADRR_TWSR    0x21
+#define     ADRR_TWBR    0x20
+
+
+
+#endif
